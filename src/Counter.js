d69d3b4d46ce2b5a291866239535c1be1487de51
lim ho <dlagh123@gmail.com>
2022-05-03 15:54:20 +0900

    .

--- src/Counter.js
+++ src/Counter.js
@@ -11,7 +11,6 @@ function Counter() {
     return (
         <div>
             <h1>{number}</h1>
-
             <button onClick={onIncrease}>+1</button>
             <button onClick={onDecrease}>-1</button>
         </div>
