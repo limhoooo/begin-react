175b31690f0eb1ed0ff7e88cef461dd902fb5714
lim ho <dlagh123@gmail.com>
2022-05-16 15:18:38 +0900

    .

--- src/Counter.js
+++ src/Counter.js
@@ -4,6 +4,7 @@ function Counter() {
     const [number, setNumber] = useState(0);
     const onIncrease = () => {
         setNumber(number + 1)
+
     }
     const onDecrease = () => {
         setNumber(number - 1)
